4b8638b4d098b6993c598c13da0fda3015d5e1a6
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-08 22:39:54 +0300

    first commit

---
+++ lib/Image.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class Image extends StatefulWidget {
+  Image({Key? key}) : super(key: key);
+
+  @override
+  State<Image> createState() => _ImageState();
+}
+
+class _ImageState extends State<Image> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
